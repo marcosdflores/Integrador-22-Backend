1894a4b57fdbdba65da3bc1cb230a4fefc05ab65
Macarena Soledad Cardozo <107225716+SOLE-VME@users.noreply.github.com>
2023-09-28 23:29:26 -0300

    Update exceptions.py

--- api/models/exceptions.py
+++ api/models/exceptions.py
@@ -1,19 +1,39 @@
-class BadRequestError(Exception):
-    def __init__(self, message="Bad Request"):
-        self.message = message
-        super().__init__(self.message)
+from flask import jsonify
 
-class NotFoundError(Exception):
-    def __init__(self, message="Not Found"):
-        self.message = message
-        super().__init__(self.message)
+class CustomException(Exception):
 
-class ForbiddenError(Exception):
-    def __init__(self, message="Forbidden"):
-        self.message = message
-        super().__init__(self.message)
+    def __init__(self, status_code, name = "Custom Error", description = 'Error'): 
+        super().__init__()
+        self.status_code = status_code
+        self.name = name
+        self.description = description
 
-class ServerError(Exception):
-    def __init__(self, message="Server Error"):
-        self.message = message
-        super().__init__(self.message)
+    def get_response(self):
+        response = jsonify({
+            'error': {
+                'code': self.status_code,
+                'name': self.name,
+                'description': self.description,
+            }
+        })
+        response.status_code = self.status_code
+        return response
+    
+class BadRequestError(CustomException):
+    def __init__(self, username, paswwordd):
+        super().__init__(status_code=400, name="Bad Request", description=f"User with username {username} not found")
+        super().__init__(status_code=400, name="Bad Request", description=f"User with paswwordd {paswwordd} not found")
+
+class NotFoundError(CustomException):
+    def __init__(self, username):
+        super().__init__(status_code=404, name="Not Found", description=f"User with username {username} not found")
+
+
+class ForbiddenError(CustomException):
+    def __init__(self, id_usuario):
+        super().__init__(status_code=403, name="Forbidden", description=f"The user {id_usuario} does not have permission to perform this action")
+
+
+class ServerError(CustomException):
+    def __init__(self, id_servidor):
+        super().__init__(status_code=500, name="Server Error", description=f"Internal Server Error{id_servidor}")
